500d913c52d239864aa4c6e04b7d93901da70af2
Диметрий Киянцев <kiyantsev90@mail.ru>
2024-08-22 16:38:26 +0400

    deployment scripts

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3010",
+      "/api": import.meta.env.API_BASE_URL,
     },
   },
   plugins: [react()],
